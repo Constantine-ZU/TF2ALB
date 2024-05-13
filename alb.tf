00a8beb9e2e67f160e936273288aee5d4da3ded9
Constantine-Z <ktb747@gmail.com>
2024-05-13 09:18:40 -0700

    aa

--- alb.tf
+++ alb.tf
@@ -62,8 +62,8 @@ data "aws_s3_object" "certificate_data" {
 
 
 resource "aws_acm_certificate" "cert" {
-  private_key       = data.aws_s3_bucket_object.private_key_data.body
-  certificate_body  = data.aws_s3_bucket_object.certificate_data.body
+  private_key       = data.aws_s3_object.private_key_data.body
+  certificate_body  = data.aws_s3_object.certificate_data.body
 
   lifecycle {
     create_before_destroy = true
