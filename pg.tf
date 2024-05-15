5045bec2794b069580c3338670f65dfa4befbb98
Constantine-Z <ktb747@gmail.com>
2024-05-14 23:30:21 -0700

    0

--- pg.tf
+++ pg.tf
@@ -1,22 +1,22 @@
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "pg_instance" {
   identifier = "mypostgresdb"
   engine = "postgres"
-  engine_version = "13.2"
+  engine_version = "16.2"
   instance_class = "db.t2.micro"
   allocated_storage = 20
   username = "dbuser"
   password = var.db_password
-  db_subnet_group_name = aws_db_subnet_group.default.name
+  db_subnet_group_name = aws_db_subnet_group.pg_subnet_group.name
   vpc_security_group_ids = [aws_security_group.pg_sg.id]
   skip_final_snapshot = true
 }
 
-resource "aws_db_subnet_group" "default" {
+resource "aws_db_subnet_group" "pg_subnet_group" {
   name = "main"
   subnet_ids = [aws_subnet.subnet_10_0.id, aws_subnet.subnet_20_0.id]
 
   tags = {
-    Name = "My DB subnet group"
+    Name = "Postgres DB subnet group"
   }
 }
 
