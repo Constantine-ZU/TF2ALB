0de4a7599d68880abf389d9699577e8b1303506c
Constantine-Z <ktb747@gmail.com>
2024-05-26 01:06:21 -0700

    qq

--- main.tf
+++ main.tf
@@ -191,6 +191,7 @@ resource "aws_instance" "Instance_20_7" {
  tags = {
     Name = "Ubuntu-ALB-10-7"
   }
+depends_on = [aws_db_instance.pg_instance]
 }
 
 resource "aws_instance" "Instance_10_6" {
@@ -250,4 +251,6 @@ resource "aws_instance" "Instance_10_6" {
   tags = {
     Name = "Ubuntu-ALB-10-6"
   }
+  depends_on = [aws_instance.pg_instance.Instance_20_7]
+
 }
